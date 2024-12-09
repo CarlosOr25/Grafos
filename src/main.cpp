fe3c743c1ade25a10a5ac154e69e09126ff44732
Carlos <CarlosOrChavez25@gmail.com>
2024-12-08 20:44:47 -0400

    Add: elections data

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,41 @@
 using namespace std;
 
 void pruebasGrafoListaAdyacencia() {
-    GrafoListaAdyacencia grafo(3);
-    grafo.agregarArista(0, 1);
-    grafo.agregarArista(1, 2);
+    int numVertices, numAristas;
+    cout << "Ingrese el numero de verties para el grafo con Lista de Adyacencia: ";
+    cin >> numVertices;
+    GrafoListaAdyacencia grafo(numVertices);
+
+    cout << "Ingrese el numero de aristas: ";
+    cin >> numAristas;
+
+    for (int i = 0; i < numAristas; ++i) {
+        int vertice1, vertice2;
+        cout << "Ingrese los verties de la arista " << i + 1 << " (formato: vertice1 vertice2): ";
+        cin >> vertice1 >> vertice2;
+        grafo.agregarArista(vertice1, vertice2);
+    }
+
     cout << "Grafo con Lista de Adyacencia:" << endl;
     grafo.imprimirGrafo();
 }
 
 void pruebasGrafoMatrizAdyacencia() {
-    GrafoMatrizAdyacencia grafo(3);
-    grafo.agregarArista(0, 1);
-    grafo.agregarArista(1, 2);
+    int numVertices, numAristas;
+    cout << "Ingrese el numero de verties para el grafo con Matriz de Adyacencia: ";
+    cin >> numVertices;
+    GrafoMatrizAdyacencia grafo(numVertices);
+
+    cout << "Ingrese el numero de aristas: ";
+    cin >> numAristas;
+
+    for (int i = 0; i < numAristas; ++i) {
+        int vertice1, vertice2;
+        cout << "Ingrese los verties de la arista " << i + 1 << " (formato: vertice1 vertice2): ";
+        cin >> vertice1 >> vertice2;
+        grafo.agregarArista(vertice1, vertice2);
+    }
+
     cout << "Grafo con Matriz de Adyacencia:" << endl;
     grafo.imprimirGrafo();
 }
